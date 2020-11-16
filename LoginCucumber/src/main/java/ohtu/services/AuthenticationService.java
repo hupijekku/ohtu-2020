e7d7b55509a5398f285732c7013ead03223ba27b
Eemeli Saarinen <saarineneemelia@gmail.com>
2020-11-16 18:04:29 +0200

    Viikko 3 Tehtävä 6

---
+++ LoginCucumber/src/main/java/ohtu/services/AuthenticationService.java
@@ -0,0 +1,62 @@
+package ohtu.services;
+
+import ohtu.domain.User;
+import java.util.ArrayList;
+import java.util.List;
+import ohtu.data_access.UserDao;
+
+public class AuthenticationService {
+
+    private UserDao userDao;
+
+    public AuthenticationService(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public boolean logIn(String username, String password) {
+        for (User user : userDao.listAll()) {
+            if (user.getUsername().equals(username)
+                    && user.getPassword().equals(password)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean createUser(String username, String password) {
+        if (userDao.findByName(username) != null) {
+            return false;
+        }
+
+        if (invalid(username, password)) {
+            return false;
+        }
+
+        userDao.add(new User(username, password));
+
+        return true;
+    }
+
+    private boolean invalid(String username, String password) {
+        // validity check of username and password
+    	if (userDao.findByName(username) != null) {
+    		return true;
+    	}
+    	if (username.length() < 3 || password.length() < 8) {
+    		return true;
+    	}
+    	for (char c : username.toCharArray()) {
+    		if ((int)c < (int)'a' || (int)c > (int)'z') {
+    			return true;
+    		}
+    	}
+    	boolean notFound = true;
+    	for (char c : password.toCharArray()) {
+    		if (!Character.isAlphabetic(c)) {
+    			notFound = false;
+    		}
+    	}
+        return notFound;
+    }
+}
